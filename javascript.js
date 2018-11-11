a7a63c4d60cac4a6695b2321b0212fbbde403c59
KumoAoiro <44928086+KumoAoiro@users.noreply.github.com>
2018-11-11 02:44:07 -0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function mudarIcone1(icone) {
+	if (icone == IconeTwitter)
+		document.getElementById("IconeTwitter").src = "images/twitterlogo2.png";
+
+	if (icone == IconeArtstation)
+		document.getElementById("IconeArtstation").src = "images/artstationlogo2.png";
+
+	if (icone == IconeInstagram)
+		document.getElementById("IconeInstagram").src = "images/instagramlogo2.png";
+}
+
+function mudarIcone2(icone) {
+	if (icone == IconeTwitter)
+		document.getElementById("IconeTwitter").src = "images/twitterlogo.png";
+
+	if (icone == IconeArtstation)
+		document.getElementById("IconeArtstation").src = "images/artstationlogo.png";
+
+	if (icone == IconeInstagram)
+		document.getElementById("IconeInstagram").src = "images/instagramlogo.png";
+}
